da4b7a5a171f2f6b40fe680ac00263ab4048f76b
Junior <junior.jaava@gmail.com>
2020-11-28 00:06:50 +0100

    encapsulating css style components

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -6,8 +6,8 @@ import { IProduct } from './product';
 
 @Component({
   selector: 'pm-products',
-  templateUrl: './product-list.component.html'
-  // styleUrls: ['./product-list.component.css']
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent 
 //implements OnInit 
